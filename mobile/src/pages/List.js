d7ad203927e9f4f7956bf1138bce65bd029fcdf0
brnlongui1 <brunorlongui@gmail.com>
2020-02-18 17:14:49 -0400

    Add Socket e comportamento

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,10 +1,21 @@
 import React, {useState, useEffect} from 'react';
-import { SafeAreaView, ScrollView, StyleSheet ,Image, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client'
+import { SafeAreaView, Alert ,ScrollView, StyleSheet ,Image, AsyncStorage } from 'react-native';
 import SpotList from '../components/SpotList';
 import logo from '../assets/logo.png'
 
 export default function List(){
     const [techs, setTechs] = useState([]);
+    
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id =>{
+            const socket = socketio('http://localhost:3333', {query:{user_id}});
+            socket.on('booking_response', booking =>{
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REPROVADA'}`);
+            });
+        });
+    });
+
     useEffect(() => {
         AsyncStorage.getItem('techs').then( storageTechs =>{
             const techsArray = storageTechs.split(',').map(tech => tech.trim());
