906dbe431868870cb6af8963e9a2b2a5dbd02cb9
brnlongui1 <brunorlongui@gmail.com>
2020-02-18 01:24:56 -0400

    criando as telas e renders

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { SafeAreaView,StyleSheet ,Image, AsyncStorage } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet ,Image, AsyncStorage } from 'react-native';
 import SpotList from '../components/SpotList';
 import logo from '../assets/logo.png'
 
@@ -13,8 +13,10 @@ export default function List(){
     }, []);
     return (
         <SafeAreaView style={styles.container}>
-            <Image style={styles.logo} source={logo}/>        
-            {techs.map(tech => <SpotList key={tech} tech={tech}/>)}
+            <Image style={styles.logo} source={logo}/>   
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech}/>)}
+            </ScrollView>
         </SafeAreaView>
     )
 }
